942ade7f27a12d449b62cd9ece6ba9e76bab9deb
Murilo Mothsin <mothsin@hotmail.com>
2013-10-15 11:09:01 -0300

    senha BD

--- class/oConn.php
+++ class/oConn.php
@@ -2,7 +2,7 @@
 $hostname_sai_principal = "localhost";
 $database_sai_principal = "sysup_newsletter";
 $username_sai_principal = "root";
-$password_sai_principal = "123";
+$password_sai_principal = "mothsin04";
 
 $sai_principal = mysql_connect($hostname_sai_principal, $username_sai_principal, $password_sai_principal) or trigger_error("Erro ao conectar no banco de dados! ".E_USER_ERROR);
 
